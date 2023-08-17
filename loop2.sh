cd675bbe98661ff04f339b7e4b10cc3c8f619b80
Vandana <rikkavandanareddy@gmail.com>
2023-08-17 22:45:30 +0530

    Addition script2

--- loop2.sh
+++ loop2.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 DATE=$(date +%F-%H-%M)
+Validate()
+{
+  if [ $1 -ne 0 ]
+then 
+    echo -e "$2  ........$R Failure $N"
+else
+    echo  -e "$2  ......$G Successful $N"
+fi  
+}
 
 for i in $@
 do
     yum install $i -y &>>/tmp/$i-$DATE.log
+    validate $? "$i installation"
 done
