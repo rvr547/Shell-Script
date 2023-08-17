8f831e6e49075967cda0e822e3cbd12819c523fe
Vandana <rikkavandanareddy@gmail.com>
2023-08-17 20:56:38 +0530

    Addition script2

--- conditions.sh
+++ conditions.sh
@@ -5,6 +5,8 @@ read A
 if [ $A -gt 10 ]
 then 
    echo "$A is greater than 10"
-else
-    echo "$A is not greater than 10"
+else if [ $A -eq 10 ]
+    echo "$A is Equal to 10"
+else 
+    echo "$A is less than 10"
 fi
