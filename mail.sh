9dc016175278725c5b7161d8405d979ebd80e908
Vandana <rikkavandanareddy@gmail.com>
2023-08-26 18:38:07 +0530

    Disk Usage

--- mail.sh
+++ mail.sh
@@ -7,5 +7,5 @@ TEAM_NAME=$4
 ALERT_TYPE=$5
 
 #echo "all args: $@"
-FBODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/" ) 
+FBODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/" template.html) 
 echo -e "$FBODY" | mail -s "$SUBJECT" $TO_ADDRESS
